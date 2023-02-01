2117d105920e537cd63b02558d203f851070eedb
NeerajGupta23 <230803neeraj@gmail.com>
2023-02-01 14:25:43 +0530

    first commit

---
+++ prg1.java
@@ -0,0 +1,7 @@
+public class prg1 {
+    public static void main(String[] args) {
+        System.out.println("Hello All");
+        System.out.println("args[0]");
+        System.out.println(args[1]);
+    }
+}
---
+++ prg2.java
@@ -0,0 +1,12 @@
+public class prg2 {
+    public static void main(String... args) {
+        System.out.println(args[1]);
+    }
+}
+
+// in the place of String args[] we can write following codes :
+// 1. static public void main(Strings []args)
+// 1. static public void main(Strings args[])
+// 1. public static void main(Strings []args)
+// 1. public static void main(Strings args[])
+// 1. static public void main(Strings... args)
